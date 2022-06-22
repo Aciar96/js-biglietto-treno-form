bb386f4fe2c494e82234035478208857e353f6d9
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-06-22 16:14:36 +0200

    fixed value

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //costanti utili 
-let userName = document.getElementById('username' , 'alfrdo')
-let kms = document.getElementById('kms', 50)
+let userName = document.getElementById('username')
+let kms = document.getElementById('kms')
 let select = document.getElementById('agerenge')
 let generetor = document.getElementById('generetor')
 
@@ -13,4 +13,31 @@ console.log('generetor')
 
 console.log(userName = userName .value)
 console.log(kms = kms .value)
-console.log(agerenge = agerenge .value)
+console.log(select = select .value )
+ 
+
+let sum = kms * 0.21 ;
+sum = sum.toFixed(2);
+console.log(sum);
+
+
+
+let hasDiscount = false;
+
+
+
+    if (select <= 17){
+    hasDiscount = true    
+    sum = sum - (sum * 20 / 100);
+    sum = sum.toFixed(2);
+   
+    console.log('hai ricevuto uno sconto del 20%' + '' + sum );
+    
+}else if ( select > 65) {
+    hasDiscount = true 
+    sum = sum - (sum * 40 / 100);
+    sum = sum.toFixed(2);
+    
+    console.log('hai ricevuto uno sconto del 40%' + '' + sum);
+}
+
