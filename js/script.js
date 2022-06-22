8bc93241735f4807c9ad6fa3ca34c9cbe820dbb4
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-06-22 15:53:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//costanti utili 
+let userName = document.getElementById('username' , 'alfrdo')
+let kms = document.getElementById('kms', 50)
+let select = document.getElementById('agerenge')
+let generetor = document.getElementById('generetor')
+
+
+
+console.log('username')
+console.log('kms')
+console.log('agerenge')
+console.log('generetor')
+
+console.log(userName = userName .value)
+console.log(kms = kms .value)
+console.log(agerenge = agerenge .value)
